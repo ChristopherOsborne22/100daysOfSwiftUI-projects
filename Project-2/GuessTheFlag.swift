fdc59c6d09322e055efb8b304601e7d0e7c6f194
Christopher Huynh <103477725+ChristopherOsborne22@users.noreply.github.com>
2023-03-09 19:09:47 +0700

    Update GuessTheFlag.swift

--- Project-2/GuessTheFlag.swift
+++ Project-2/GuessTheFlag.swift
@@ -1,1 +1,98 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Dinh Huynh Chanh from 28/02/2023 to 9/3/2023.
+//  This is the project 2 of Paul Hudson's 100 Days of SwiftUI. 
 
+import SwiftUI
+
+struct ContentView: View {
+    @State private var showingScore = false
+    // showing alert
+    @State private var scoreTitle = ""
+    // checking if the user's choice is "Correct" or "Wrong"
+    @State private var scoreCount = 0
+    // user's score count
+    
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    // shuffled() for shuffling the arr after each preview
+    @State private var correctAnswer = Int.random(in: 0...2)
+    // the user has to choose the correct one out of the first 3 flags of array
+    
+    var body: some View {
+        ZStack {
+            LinearGradient(colors: [.pink, .orange], startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+            
+            VStack {
+                Text("Guess The Flag")
+                    .font(.largeTitle.bold()) // shorthanded for .largeTitle.weight(.bold)
+                    .foregroundColor(.white)
+                    .padding(40)
+                
+                VStack(spacing: 30) {
+                    VStack {
+                        Text("Choose the flag of")
+                            .foregroundStyle(.secondary)
+                            .font(.system(size: 35, weight: .light))
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.weight(.bold))
+                        // largeTitle is large size but dependent on the user's
+                        // device display,
+                        // weight is the "size", like heavy, thin, ultra light and bold
+                    }
+                    
+                    ForEach(0..<3) { number in
+                        Button {
+                            showingScore = true
+                            
+                            if number == correctAnswer {
+                                scoreTitle = "Correct! You are so good!"
+                                scoreCount += 1
+                            } else {
+                                scoreTitle = "Wrong! Better luck next time."
+                            }
+                        } label: {
+                            Image(countries[number])
+                                .renderingMode(.original)
+                                .clipShape(Capsule())
+                            // clipShape changes the size of the images
+                            // Capsule() makes the edges round
+                                .shadow(radius: 8)
+                            // makes shadowy background for images
+                        }
+                    }
+                }
+                .frame(width: 350, height: 500)
+                .padding(.vertical, 35)
+                .background(.ultraThinMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                
+                Text("Score: \(scoreCount)")
+                    .foregroundColor(.white)
+                    .font(.system(size: 35))
+                    .padding(40)
+            }
+        }
+        // alert at the end of ZStack
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: continueGame)
+        }
+        // alert shows whether the user chooses correct,
+        // continues the game, and displays the score
+    }
+    
+    func continueGame() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        // we can change the above values because they are
+        // @State properties
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
